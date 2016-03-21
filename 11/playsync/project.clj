fccc5eeeb49458e1e6d74859af294ccc1ab06acf
Diego Zamboni <diego.zamboni@swisscom.com>
2016-03-21 23:47:02 +0100

    Fixed for Clojure 1.8

--- 11/playsync/project.clj
+++ 11/playsync/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :main ^:skip-aot playsync.core
   :target-path "target/%s"
